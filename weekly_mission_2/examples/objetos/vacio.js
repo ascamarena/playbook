963c2d0dcb6aa86686646efa49e955f54b242f1b
Alexis Santibáñez Camarena <92229619+ascamarena@users.noreply.github.com>
2022-04-20 11:23:40 -0500

    Create vacio.js

---
+++ weekly_mission_2/examples/objetos/vacio.js
@@ -0,0 +1,18 @@
+//Vamos a crear varios objetos vacíos a partir de los ejemplos dados por el MC
+
+// Ejemplo 1: Crear un objeto
+const myObjetc = {} // Esto es un objeto vacío
+console.log("Ejemplo 1: Crear un objeto vacío")
+console.log(myObjetc)
+
+// Ejemplo 1.1: Crear objeto vacío
+const myName = {}
+console.log(myName)
+
+//Ejemplo 1.2: Mas objetos vacíos
+const myPet = {}
+console.log(myPet)
+
+//Ejemplo 1.3: Objeto vacío
+const myCity = {}
+console.log(myCity)
