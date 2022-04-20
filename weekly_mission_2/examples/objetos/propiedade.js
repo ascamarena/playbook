a7d42f578d9eb90d0140fd9c2aacbded29816d04
Alexis Santibáñez Camarena <92229619+ascamarena@users.noreply.github.com>
2022-04-20 11:28:19 -0500

    Create propiedade.js

---
+++ weekly_mission_2/examples/objetos/propiedade.js
@@ -0,0 +1,16 @@
+// Ejemplo 2: Crear un objeto con propiedades
+const myObjetc2 = {
+  name: "Carlo",
+  age: 27
+}
+
+// Ejemplo 2.1: Otro objeto con propiedades
+const myRoom = {
+  color: "Blue",
+  illumination: false,
+}
+
+// Ejemplo 2.2: Llamar al color de myRoom
+console.log(myRoom.color)
+
+// Ejemplo 2.3: function
