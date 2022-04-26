b942aa42af95dd064796e3fbf41fca436685230f
Alexis Santibáñez Camarena <92229619+ascamarena@users.noreply.github.com>
2022-04-26 13:55:23 -0500

    npm init, start, test

---
+++ weekly_mission_3/exercises/exercise1/my_launchx_app/test/index.js
@@ -0,0 +1,10 @@
+const MissionComander = require('../app/missionComander');
+
+const carlo = new MissionComander("Carlo");
+console.log(carlo.name)
+
+const fer8a = new MissionComander("Fer");
+console.log(fer8a.name)
+
+const jesus = new MissionComander("Jesús");
+console.log(jesus.name)
